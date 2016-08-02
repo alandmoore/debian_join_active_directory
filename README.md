e923f2eeaa88f9774d9a89646c8a26c92d15f264
Alan D Moore <me@alandmoore.com>
2016-08-02 09:36:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Debian AD Join Script #
+
+This script was created to simplify adding a debian system to Active Directory.  It started with [a blog post](http://www.alandmoore.com/blog/2015/05/06/joining-debian-8-to-active-directory/), and it seemed there was a need to improve it so I've created this github repository.
+
+Please feel free to make pull requests to fix problems you encounter joining your AD.
+
+## Goals ##
+
+This script should:
+
+- Join a base install of Debian Stable to Active Directory
+- Allow domain logins
+- Allow Domain Admins to use `sudo`
+
+Additional features would be nice.
---
+++ join_active_directory.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script should join Debian Jessie (8) to an Active Directory domain.
+echo "Please authenticate with your sudo password"
+sudo -v
+
+if ! $(sudo which realmd 2>/dev/null); then
+    sudo aptitude install realmd adcli sssd
+fi
+
+if ! $(sudo which ntpd 2>/dev/null); then
+    sudo aptitude install ntp
+fi
+
+sudo mkdir -p /var/lib/samba/private
+
+echo "Please enter the domain you wish to join: "
+read DOMAIN
+
+echo "Please enter a domain admin login to use: "
+read ADMIN
+
+sudo realm join --user=$ADMIN $DOMAIN
+
+if [ $? -ne 0 ]; then
+    echo "AD join failed.  Please run 'journalctl -xn' to determine why."
+    exit 1
+fi
+
+sudo systemctl enable sssd
+sudo systemctl start sssd
+
+echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=0022" | sudo tee -a /etc/pam.d/common-session
+
+# configure sudo
+sudo aptitude install libsss-sudo
+
+echo "%domain\ admins@$DOMAIN ALL=(ALL) ALL" | sudo tee -a /etc/sudoers.d/domain_admins
+
+echo "The computer is joined to the domain.  Please reboot, ensure that you are connected to the network, and you should be able to login with domain credentials."
