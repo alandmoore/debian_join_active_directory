642e74ed0b3cd2e2bc6680683b42ec30f4796570
Alan D Moore <me@alandmoore.com>
2016-08-02 11:46:15 -0500

    PM command changeable, sssd.conf
    - Make PM install command easily changeable
    - Touch sssd.conf to fix problem where it's not auto-generated

--- join_active_directory.sh
+++ join_active_directory.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Feel free to replace with your favorite PM frontend
+INSTALL_CMD="aptitude install"
+
+
 # This script should join Debian Jessie (8) to an Active Directory domain.
 echo "Please authenticate with your sudo password"
 sudo -v
 
 if ! $(sudo which realmd 2>/dev/null); then
-    sudo aptitude install realmd adcli sssd
+    sudo $INSTALL_CMD realmd adcli sssd
 fi
 
 if ! $(sudo which ntpd 2>/dev/null); then
-    sudo aptitude install ntp
+    sudo $INSTALL_CMD install ntp
 fi
 
 sudo mkdir -p /var/lib/samba/private
@@ -27,13 +31,16 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# This step was necessary for some users, since this file isn't always created.
+sudo touch /etc/sssd/sssd.conf
+
 sudo systemctl enable sssd
 sudo systemctl start sssd
 
 echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=0022" | sudo tee -a /etc/pam.d/common-session
 
 # configure sudo
-sudo aptitude install libsss-sudo
+sudo $INSTALL_CMD libsss-sudo
 
 echo "%domain\ admins@$DOMAIN ALL=(ALL) ALL" | sudo tee -a /etc/sudoers.d/domain_admins
 
